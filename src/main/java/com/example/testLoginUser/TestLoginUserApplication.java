46ab70eb8cf20d157033ec7ff76f2e76c0872268
francescoriccardo <francescoriccardo1988@gmail.com>
2018-09-27 11:35:25 +0200

    first commit

---
+++ src/main/java/com/example/testLoginUser/TestLoginUserApplication.java
@@ -0,0 +1,12 @@
+package com.example.testLoginUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestLoginUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLoginUserApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/testLoginUser/TestLoginUserApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testLoginUser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestLoginUserApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
